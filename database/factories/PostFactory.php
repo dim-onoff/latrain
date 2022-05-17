2c6569cdc2de5c618781633b073d896c31c97e5e
D F <frieddimon@web.de>
2022-05-17 22:00:36 +0200

    initial commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => $this->faker->numberBetween(1,100),
+            'title' => $this->faker->sentence,
+            'body' => $this->faker->text(),
+            'created_at' => $this->faker->dateTimeBetween('2020-01-01','2022-12-31'),
+            'deleted_at' => $this->faker->randomElement([
+                null,
+                $this->faker->dateTimeBetween('2020-01-01','2022-12-31'),
+            ])
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory(10)
+            ->has(Post::factory()->count(10))
+            ->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
