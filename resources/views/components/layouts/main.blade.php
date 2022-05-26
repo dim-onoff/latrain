dbfe4fe4b7040b7e95b696eddc5174d6244fba0e
D F <frieddimon@web.de>
2022-05-26 21:31:15 +0200

    add tailwind

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -7,7 +7,8 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.4/tailwind.min.css" integrity="sha512-paVHVRRhdoOu1nOXKnqDC1Vka0nh7FAmU3nsM4n2FKxOQTeF6crMdMfkVvEsuaOXZ6oEAVL5+wLbQcule/Xdag==" crossorigin="anonymous" />    <style>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <style>
         body {
             font-family: 'Nunito', sans-serif;
         }
@@ -16,7 +17,7 @@
 <body class="bg-gray-100 min-h-screen flex flex-col">
 <x-layouts.header />
 <main class="flex flex-1 container max-w-7xl mx-auto px-5 lg:px-40 space-x-5 mb-5">
-    {{$slot}}
+    {{ $slot }}
     <div class="space-y-5" style="width:300px;">
 
         <div class="bg-blue-700 shadow rounded-sm">
@@ -24,17 +25,19 @@
             <div class="border-t border-blue-600 text-white p-5 text-sm">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum laboriosam omnis repudiandae unde.
                 <div class="flex mt-5">
-                    <input type="text" name="newsletter" class="rounded-l-sm text-gray-800 px-4 py-2 h-12" placeholder="E-Mail Adresse" style="min-width: 0px;">
+                    <input type="text" name="newsletter" class="rounded-l-sm text-gray-800 px-4 py-2 h-12" placeholder="E-Mail Adresse"
+                           style="min-width: 0px;">
                     <button type="button" class="bg-blue-800 h-12 text-xs px-4 py-2 rounded-r-sm">abonnieren</button>
                 </div>
             </div>
         </div>
 
         <x-post.newest/>
-        <x-post.categories/>
-        <x-post.trending/>
+        <x-post.categories />
+        <x-post.trending />
     </div>
 </main>
 <x-layouts.footer />
+<script src="{{asset('js/app.js')}}" type="text/javascript"></script>
 </body>
 </html>
