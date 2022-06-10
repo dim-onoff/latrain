eb6ae4a2ea967478717d0099177e8d81a5c9336e
D F <frieddimon@web.de>
2022-06-10 21:58:28 +0200

    Add list view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\StartpageController;
+use App\Http\Controllers\Backend\PostController as BackendPostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +22,7 @@ Route::middleware(['auth'])->group(function () {
     Route::prefix('admin')->group(function () {
         Route::name('admin.')->group(function () {
             Route::view('/', 'backend.index')->name('index');
+            Route::resource('/post', BackendPostController::class);
         });
     });
 });
