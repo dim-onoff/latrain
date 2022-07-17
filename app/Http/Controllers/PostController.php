d5e0c4df98bb928653470bbc9b451d4f4d0e0dd3
D F <frieddimon@web.de>
2022-07-17 20:53:32 +0200

    add Premium Content show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -54,6 +54,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
+        if($post->is_premium) $this->authorize('show premium-posts');
         return view('backend.post.show', compact('post'));
     }
 
